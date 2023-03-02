25698bf402f59f2e9892dec9ef56e6d73180d676
pvczku <patryk.kulma.dev@gmail.com>
2023-03-02 17:36:59 +0000

    init

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const PORT = 3000;
 require("colors");
 const logger = require("tracer").colorConsole();
 const server = http.createServer((req, res) => {});
+const {Server} = require('socket.io')
 
 server.listen(PORT, () => logger.info("app launched on port " + PORT));
